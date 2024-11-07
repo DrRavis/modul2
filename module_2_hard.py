d75136803dfbd78fa96ab89108780f34006ff215
Ravis Ratesey <162020276+DrRavis@users.noreply.github.com>
2024-11-07 15:01:49 +0300

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,15 @@
+number = int(input('Число для ввода: '))
+result = ''
+for a in range(1, 20):
+    for b in range(2, 20):
+        sum = a + b
+        if number % sum == 0 and a < b:
+            result = result + str(a) + str(b)
+        else:
+            continue
+print(result)
+check = input('Для проверки: ')
+if check == result:
+    print('Всё правильно!')
+else:
+    print('Где-то ошибка')
