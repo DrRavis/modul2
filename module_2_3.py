66f0949fb7a540b9a631e01b280e927e3abbfde2
Ravis Ratesey <162020276+DrRavis@users.noreply.github.com>
2024-11-16 18:01:00 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,8 +1,8 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-a = 0
-while a < len(my_list):
-    if my_list[a] > 0:
-        print(my_list[a])
-    elif my_list[a] < 0:
+number = 0
+while number < len(my_list):
+    if my_list[number] > 0:
+        print(my_list[number])
+    elif my_list[number] < 0:
         break
-    a = a + 1
+    number = number + 1
